bd377e2493ee7377345c9cdbe78ceae0477aa99d
qianli <991013842@qq.com>
2018-10-30 22:56:35 +0800

    +++框架升级

--- titan_boot/src/main/java/com/cjwx/titan/boot/BootApplication.java
+++ titan_boot/src/main/java/com/cjwx/titan/boot/BootApplication.java
@@ -4,15 +4,14 @@ import com.cjwx.titan.engine.core.constant.HttpConstant;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.util.ArrayList;
+import java.util.Arrays;
 
 @SpringBootApplication
 public class BootApplication {
 
     public static void main(String[] args) {
         HttpConstant.VERSION = "0.0.1";
-        HttpConstant.EXCLUSIONS = new ArrayList<>();
-        HttpConstant.EXCLUSIONS.add("/system/user/token");
+        HttpConstant.EXCLUSIONS = Arrays.asList("/system/user/token");
         SpringApplication.run(BootApplication.class, args);
     }
 
