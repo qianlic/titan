6d588443e860961dfa6a7972ba2e67bad3796919
qianli <991013842@qq.com>
2018-12-28 15:24:20 +0800

    +++

--- titan_server/src/main/java/com/cjwx/titan/server/config/ServerConfiguration.java
+++ titan_server/src/main/java/com/cjwx/titan/server/config/ServerConfiguration.java
@@ -2,6 +2,10 @@ package com.cjwx.titan.server.config;
 
 import com.cjwx.titan.engine.config.ApplicationConfiguration;
 import com.cjwx.titan.engine.config.WebMvcConfiguration;
+import com.cjwx.titan.engine.util.file.FileTool;
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
@@ -11,9 +15,32 @@ import org.springframework.context.annotation.Import;
  * @Author: qian li
  * @Date: 2018年11月03日 10:20
  */
+@Data
 @Configuration
+@ConfigurationProperties("ftp")
 @Import({ApplicationConfiguration.class, WebMvcConfiguration.class})
 @ComponentScan("com.cjwx.titan.server.zuul")
 public class ServerConfiguration {
+
+    private String ftpURL;
+    private Integer ftpPort;
+    private String ftpAdmin;
+    private String ftpPassword;
+    private String ftpRootPath;
+    private Long maxFileSize;
+    private String fileFormat;
+
+    @Bean
+    public FileTool fileTool() {
+        FileTool fileTool = new FileTool();
+        fileTool.setFtpURL(ftpURL);
+        fileTool.setFtpPort(ftpPort);
+        fileTool.setFtpAdmin(ftpAdmin);
+        fileTool.setFtpPassword(ftpPassword);
+        fileTool.setFtpRootPath(ftpRootPath);
+        fileTool.setMaxFileSize(maxFileSize);
+        fileTool.setFileFormat(fileFormat);
+        return fileTool;
+    }
 
 }
