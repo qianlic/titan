187093917c74be797303a8772b74868a049590dd
qianli <991013842@qq.com>
2018-12-28 15:18:11 +0800

    +++

---
+++ titan_server/src/main/java/com/cjwx/titan/server/config/CaptchaConfiguration.java
@@ -0,0 +1,47 @@
+package com.cjwx.titan.server.config;
+
+import com.cjwx.titan.server.capcha.CaptchaConfig;
+import com.cjwx.titan.server.capcha.CaptchaHepler;
+import com.google.code.kaptcha.impl.DefaultKaptcha;
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Properties;
+
+/**
+ * @Description:
+ * @Author: qian li
+ * @Date: 2018年11月03日 10:20
+ */
+@Data
+@Configuration
+@ConfigurationProperties("kaptcha")
+public class CaptchaConfiguration {
+
+    private String border;
+    private String width;
+    private String height;
+    private String font;
+    private String noise;
+    private String chars;
+
+    @Bean
+    public DefaultKaptcha captchaProducer() {
+        Properties properties = new Properties();
+        properties.setProperty("kaptcha.border", border);
+        properties.setProperty("kaptcha.image.width", width);
+        properties.setProperty("kaptcha.image.height", height);
+        properties.setProperty("kaptcha.textproducer.font.names", font);
+        properties.setProperty("kaptcha.noise.impl", noise);
+        properties.setProperty("kaptcha.textproducer.char.string", chars);
+        CaptchaConfig config = new CaptchaConfig(properties);
+
+        DefaultKaptcha kaptcha = new DefaultKaptcha();
+        kaptcha.setConfig(config);
+        CaptchaHepler.setCaptchaProducer(kaptcha);
+        return kaptcha;
+    }
+
+}
---
+++ titan_server/src/main/java/com/cjwx/titan/server/config/WxmpConfiguration.java
@@ -0,0 +1,48 @@
+package com.cjwx.titan.server.config;
+
+import lombok.Data;
+import me.chanjar.weixin.mp.api.WxMpInMemoryConfigStorage;
+import me.chanjar.weixin.mp.api.WxMpMessageRouter;
+import me.chanjar.weixin.mp.api.WxMpServiceImpl;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @Description:
+ * @Author: qian li
+ * @Date: 2018年11月03日 10:20
+ */
+@Data
+@Configuration
+@ConfigurationProperties("wxmp")
+public class WxmpConfiguration {
+
+    private String appId;
+    private String secret;
+    private String token;
+    private String aesKey;
+
+    @Bean
+    public WxMpInMemoryConfigStorage wxMpConfigStorage() {
+        WxMpInMemoryConfigStorage config = new WxMpInMemoryConfigStorage();
+        config.setAppId(appId);
+        config.setSecret(secret);
+        config.setToken(token);
+        config.setAesKey(aesKey);
+        return config;
+    }
+
+    @Bean
+    public WxMpServiceImpl wxMpService(WxMpInMemoryConfigStorage wxMpConfigStorage) {
+        WxMpServiceImpl service = new WxMpServiceImpl();
+        service.setWxMpConfigStorage(wxMpConfigStorage);
+        return service;
+    }
+
+    @Bean
+    public WxMpMessageRouter wxMpMessageRouter(WxMpServiceImpl wxMpService) {
+        return new WxMpMessageRouter(wxMpService);
+    }
+
+}
