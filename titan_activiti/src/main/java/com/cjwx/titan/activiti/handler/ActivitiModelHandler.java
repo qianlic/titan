95bcb1672827d8deeaf280fe207586a34ce40541
qianli <991013842@qq.com>
2020-04-28 16:23:18 +0800

    +++

--- titan_activiti/src/main/java/com/cjwx/titan/activiti/handler/ActivitiModelHandler.java
+++ titan_activiti/src/main/java/com/cjwx/titan/activiti/handler/ActivitiModelHandler.java
@@ -3,7 +3,10 @@ package com.cjwx.titan.activiti.handler;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import lombok.extern.slf4j.Slf4j;
+import org.activiti.editor.constants.ModelDataJsonConstants;
 import org.activiti.engine.ActivitiException;
+import org.activiti.engine.ProcessEngine;
+import org.activiti.engine.ProcessEngines;
 import org.activiti.engine.RepositoryService;
 import org.activiti.engine.repository.Model;
 import org.apache.batik.transcoder.TranscoderInput;
@@ -16,6 +19,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.InputStream;
@@ -27,6 +32,7 @@ import java.io.InputStream;
  */
 @Slf4j
 @RestController
+@RequestMapping("/activiti/")
 public class ActivitiModelHandler {
 
     @Autowired
@@ -100,8 +106,45 @@ public class ActivitiModelHandler {
         }
     }
 
+    @RequestMapping("/model/create")
+    public void createModel(HttpServletRequest request, HttpServletResponse response) {
+        try {
+            String modelName = "modelName";
+            String modelKey = "modelKey";
+            String description = "description";
+
+            ProcessEngine processEngine = ProcessEngines.getDefaultProcessEngine();
+
+            RepositoryService repositoryService = processEngine.getRepositoryService();
+
+            ObjectMapper objectMapper = new ObjectMapper();
+            ObjectNode editorNode = objectMapper.createObjectNode();
+            editorNode.put("id", "canvas");
+            editorNode.put("resourceId", "canvas");
+            ObjectNode stencilSetNode = objectMapper.createObjectNode();
+            stencilSetNode.put("namespace", "http://b3mn.org/stencilset/bpmn2.0#");
+            editorNode.put("stencilset", stencilSetNode);
+            Model modelData = repositoryService.newModel();
+
+            ObjectNode modelObjectNode = objectMapper.createObjectNode();
+            modelObjectNode.put(ModelDataJsonConstants.MODEL_NAME, modelName);
+            modelObjectNode.put(ModelDataJsonConstants.MODEL_REVISION, 1);
+            modelObjectNode.put(ModelDataJsonConstants.MODEL_DESCRIPTION, description);
+            modelData.setMetaInfo(modelObjectNode.toString());
+            modelData.setName(modelName);
+            modelData.setKey(modelKey);
+
+            //保存模型
+            repositoryService.saveModel(modelData);
+            repositoryService.addModelEditorSource(modelData.getId(), editorNode.toString().getBytes("utf-8"));
+            response.sendRedirect(request.getContextPath() + "/modeler.html?modelId=" + modelData.getId());
+        } catch (Exception e) {
+        }
+    }
+
+    @ResponseBody
     @RequestMapping(value = "/editor/stencilset", method = RequestMethod.GET, produces = "application/json;charset=utf-8")
-    public @ResponseBody String getStencilset() {
+    public String getStencilset() {
         InputStream stencilsetStream = this.getClass().getClassLoader().getResourceAsStream("stencilset.json");
         try {
             return IOUtils.toString(stencilsetStream, "utf-8");
